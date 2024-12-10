86e208ec3469045a3eea5c97fafefe7f5cbeb6c0
Vinicius Guima <vinicius.guimaraes@nowmed.com.br>
2024-12-10 19:53:57 -0300

    chore: use authorization on private api

--- spec/support/rspec_api_support.rb
+++ spec/support/rspec_api_support.rb
@@ -4,4 +4,8 @@ module RspecApiSupport
   def json(body)
     JSON.parse(body, symbolize_names: true)
   end
+
+  def auth_headers(user = nil, headers = {})
+    { 'Authorization' => "Bearer #{Pinpeople::JsonWebToken.encode(sub: user&.id)}" }.merge(headers)
+  end
 end
