0b96f185e79a6f7bf4b715990421f1e6b33c04e6
Vinicius Guima <vinicius.guimaraes@nowmed.com.br>
2024-12-11 02:33:44 -0300

    feat: complete survey flow

--- app/serializers/survey_serializer.rb
+++ app/serializers/survey_serializer.rb
@@ -26,9 +26,8 @@ class SurveySerializer
       title: survey.title,
       created_at: survey.created_at,
       updated_at: survey.updated_at,
-      questions: QuestionSerializer.new(survey.questions).serializable_hash,
-      options: OptionSerializer.new(survey.options).serializable_hash,
-      company: CompanySerializer.new(survey.company).serializable_hash
+      company: CompanySerializer.new(survey.company).serializable_hash,
+      questions: QuestionSerializer.new(survey.questions).serializable_hash
     }
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,6 @@
 # frozen_string_literal: true
 
 Rails.application.routes.draw do
-  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
-  # Reveal health status on /up that returns 200 if the app boots with no exceptions, otherwise 500.
-  # Can be used by load balancers and uptime monitors to verify that the app is live.
   get 'up' => 'rails/health#show', as: :rails_health_check
 
   namespace :api do
@@ -15,9 +12,8 @@ Rails.application.routes.draw do
 
     resources :companies do
       put :restore, on: :member
+
+      resources :surveys, only: [:create], module: :companies
     end
   end
-
-  # Defines the root path route ("/")
-  # root "posts#index"
 end
